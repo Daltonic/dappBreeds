7d5fbe1ac2b48d350b4fadbe2777bb8838654a78
Abraham covenant <Abrahamcovenant2004@gmail.com>
2023-07-14 16:06:32 +0100

    update

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,30 +1,42 @@
-import { createGlobalState } from "react-hooks-global-state";
+import { createGlobalState } from 'react-hooks-global-state'
 
 const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
-  connectedAccount: "",
+  connectedAccount: '',
   nfts: [],
-  nft : null,
-  trait: null,
-  owner: null,
-});
+  breeds: [],
+})
 
 const truncate = (text, startChars, endChars, maxLength) => {
   if (text.length > maxLength) {
-    let start = text.substring(0, startChars);
-    let end = text.substring(text.length - endChars, text.length);
+    let start = text.substring(0, startChars)
+    let end = text.substring(text.length - endChars, text.length)
     while (start.length + end.length < maxLength) {
-      start = start + ".";
+      start = start + '.'
     }
-    return start + end;
+    return start + end
   }
-  return text;
-};
+  return text
+}
 
+const addToLab = (nft) => {
+  const breeds = getGlobalState('breeds')
+  if (breeds.length == 2) return
+  setGlobalState('breeds', [nft, ...breeds])
+}
 
+const remFromLab = (nft) => {
+  const breeds = getGlobalState('breeds')
+  const index = breeds.findIndex((breed) => breed.tokenId == nft.tokenId)
+  breeds.splice(index, 1)
+  setGlobalState('breeds', [...breeds])
+}
 
 export {
   setGlobalState,
   useGlobalState,
   getGlobalState,
   truncate,
-};
+  addToLab,
+  remFromLab,
+  truncate,
+}
